1b05cbd497a7092611b6e3e0c87d59837e9ea0e1
Hwanyeong815 <your_email@example.com>
2025-06-12 19:52:45 +0900

    0612

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ const mainBanner = () => {};
 //이벤트배너
 const eventBanner = () => {};
 
+const sectionPage = () => {
+    const $con1 = get('.main .con1');
+    const $con2 = get('.main .con2');
+    const $con3 = get('.main .con3');
+    const $con4 = get('.main .con4');
+    // console.log($con1.offsetTop);
+    // 스크롤 높이 1080
+    // console.log($con2.offsetTop);
+    // 스크롤 높이 2105
+    // console.log($con3.offsetTop);
+    // 스크롤 높이 3157
+    // console.log($con4.offsetTop);
+    // 스크롤 높이 4237
+    window.scrollTo({ top: $con2.offTop, behavior: 'smooth' });
+};
+
 const menuBar = () => {
     const $menu = get('.menu');
 
@@ -23,6 +39,7 @@ const mainInit = () => {
     mainBanner();
     eventBanner();
     menuBar();
+    sectionPage();
 };
 
 (() => {
